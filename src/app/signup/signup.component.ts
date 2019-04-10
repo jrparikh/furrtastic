d14a8cb8ae802f712eab4773b1e0834c77a76df7
Jay Parikh <jrparikh@ucsc.edu>
2019-04-10 14:09:11 -0500

    Doing the input logging

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
@@ -6,6 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+
+  @Input()
+  fname: string;
+  lname: string;
+  email: string;
+  phoneNum: string;
+  address: string;
+  state: string;
+  city: string;
+  zipCode: string;
+  username: string;
+  password: string;
 
   constructor() { }
 
