447215c6ce717c5a3eb9ffefa02bb0e569da49da
Jay Parikh <jrparikh@ucsc.edu>
2019-04-18 23:04:10 -0500

    button

--- src/app/userpage/userpage.component.ts
+++ src/app/userpage/userpage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-userpage',
@@ -10,13 +11,16 @@ export class UserpageComponent implements OnInit {
   user = JSON.parse(localStorage.getItem('User'));
 
   petArr = JSON.parse(localStorage.getItem('petArr'));
-  
-  constructor() { }
+  pet;
+  constructor(private router: Router) { }
 
   ngOnInit() {
     console.log("petArr: ", this.petArr);
     console.log("petArr[0]: ", this.petArr[0]);
   }
 
-
+  buyInsurance(pet: any){
+      console.log(pet.name);
+      this.router.navigate(['/coverage']);
+  }
 }
